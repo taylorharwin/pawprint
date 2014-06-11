813f253b2d95a9d101ac435bc5b2af3fffe64f8a
emiidee <emily.y.dong@gmail.com>
2014-06-11 16:12:04 -0700

    Shortened field lengths as appropriate and corrected a foreign key definition

--- server/main/db_config.js
+++ server/main/db_config.js
@@ -22,18 +22,18 @@ db.knex.schema.hasTable('user').then(function(exists) {
   if (!exists) {
     db.knex.schema.createTable('user', function (user) {
       user.increments('id').primary();
-      user.string('email', 255);
-      user.string('firstName', 255);
-      user.string('lastName', 255);
+      user.string('email', 100);
+      user.string('firstName', 50);
+      user.string('lastName', 50);
       user.string('password', 255);
       user.string('salt', 255);
-      user.string('streetAddress', 255);
-      user.string('city', 255);
+      user.string('streetAddress', 100);
+      user.string('city', 100);
       user.string('state', 2);
       user.string('zip', 10);             // string/int
       user.string('phone', 11);           // string/int
       user.string('signature', 255);      // what is this?
-      user.integer('vet_id')
+      user.integer('vet_id');
       user.timestamps();
     }).then(function (table) {
       console.log('Created Table', table);
@@ -58,14 +58,14 @@ db.knex.schema.hasTable('pet').then(function(exists) {
   if (!exists) {
     db.knex.schema.createTable('pet', function (pet) {
       pet.increments('id').primary();
-      pet.string('name', 255);
+      pet.string('name', 50);
       pet.date('birthdate');
-      pet.string('gender', 255);
+      pet.string('gender', 1);
       pet.string('breed', 255);
-      pet.string('color', 255); 
+      pet.string('color', 50); 
       pet.integer('weight');
-      pet.boolean('neuter');              // to confirm
-      pet.string('microchip', 255);       // confirm string
+      pet.boolean('neuter');
+      pet.string('microchip', 255);
       pet.string('profilePic', 255);
       pet.timestamps();
     }).then(function (table) {
@@ -78,7 +78,7 @@ db.knex.schema.hasTable('vaccine').then(function(exists) {
   if (!exists) {
     db.knex.schema.createTable('vaccine', function (vaccine) {
       vaccine.increments('id').primary();
-      vaccine.string('name', 255);
+      vaccine.string('name', 100);
       vaccine.date('expiration');
       vaccine.timestamps();
     }).then(function (table) {
@@ -135,10 +135,10 @@ db.knex.schema.hasTable('contactHistory').then(function(exists) {
     db.knex.schema.createTable('contactHistory', function (history) {
       history.increments('id').primary();
       history.integer('admin_id');
-      history.string('type', 255);
+      history.string('type', 100);
       history.string('notes', 255);
       history.integer('request_id');
-      history.string('vetContact_id', 255);
+      history.string('vetContact_id');
       history.timestamps();
     }).then(function (table) {
       console.log('Created Table', table);
@@ -150,7 +150,7 @@ db.knex.schema.hasTable('admin').then(function(exists) {
   if (!exists) {
     db.knex.schema.createTable('admin', function (admin) {
       admin.increments('id').primary();
-      admin.string('email', 255);
+      admin.string('email', 100);
       admin.string('password', 255);
       admin.string('salt', 255);
       admin.timestamps();
@@ -166,12 +166,12 @@ db.knex.schema.hasTable('vet').then(function(exists) {
       vet.increments('id').primary();
       vet.string('practiceName', 255);
       vet.string('website', 255);
-      vet.string('streetAddress', 255);
-      vet.string('city', 255);
+      vet.string('streetAddress', 100);
+      vet.string('city', 100);
       vet.string('state', 2);               // added this 
       vet.string('zip', 10);                // string/int
       vet.string('phone', 11);              // string/int
-      vet.string('contactMethod', 255);    // integer instead?
+      vet.string('contactMethod', 100);    // integer instead?
       vet.timestamps();
     }).then(function (table) {
       console.log('Created Table', table);
@@ -183,9 +183,9 @@ db.knex.schema.hasTable('vetContact').then(function(exists) {
   if (!exists) {
     db.knex.schema.createTable('vetContact', function (vetContact) {
       vetContact.increments('id').primary();
-      vetContact.string('name', 255);
+      vetContact.string('name', 50);
       vetContact.string('title', 255);
-      vetContact.string('email', 255);
+      vetContact.string('email', 100);
       vetContact.string('phone', 11);
       vetContact.integer('vet_id');
       vetContact.timestamps();
