898a02f55104e158ffba9da3a9431f67376a6a7a
Jonathan Huang <jon.hc.huang@gmail.com>
2014-06-11 11:26:31 -0700

    Modified scaffold code to properly deploy basic server on node run. Added basic knex/bookshelf/pg connection.

--- server/main/config.js
+++ server/main/config.js
@@ -1,32 +1,37 @@
 "use strict";
 
-var mysql    = require('mysql'),
+var knex        = require('knex'),
+    bookshelf    = require('bookshelf'),
     morgan      = require('morgan'),
     bodyParser  = require('body-parser'),
     middle      = require('./middleware');
 
-var dbConnection = mysql.createConnection({
-  user: 'root',
-  password: '',
-  database: ''
-});
-
-dbConnection.connect(function(err){
-  if (!err){
-    console.log('connected to mysql!');
+var knex = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'pawprint',
+    password: 'password',
+    database: 'pawprint',
+    charset: 'utf8',
   }
 });
+
+var db = bookshelf(knex);
+
 /*
  * Include all your global env variables here.
 */
 module.exports = exports = function (app, express, routers) {
   app.set('port', process.env.PORT || 9000);
   app.set('base url', process.env.URL || 'http://localhost');
+  app.set('db', db);
   app.use(morgan('dev'));
+  // bodyParser security concerns - think about using
   app.use(bodyParser());
   app.use(middle.cors);
   app.use(express.static(__dirname + '/../../client'));
-  app.use('/note', routers.NoteRouter);
+  app.use('/extra', routers.ExtraRouter);
   app.use(middle.logError);
   app.use(middle.handleError);
 };
