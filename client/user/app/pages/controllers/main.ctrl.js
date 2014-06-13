a8f59932517413d7320d4258093d514b064b53b0
Benjamin Lee <leebyp@gmail.com>
2014-06-13 16:24:27 -0700

    Added dummy data for 3 pets

--- client/user/app/pages/controllers/main.ctrl.js
+++ client/user/app/pages/controllers/main.ctrl.js
@@ -5,29 +5,146 @@ angular.module('user.pages.controllers')
     // @TODO retrieve properly though GET request
     $scope.pets = [
       {
-        name: 'Apple',
-        age: 3,
-        imgUrl: 'http://cl.jroo.me/z3/Q/8/F/e/a.baa-Puppy-or-teddy-bear.jpg',
-        vet: 'Charlie',
-        vaccineStatus: 'active',
-        updateStatus: 'normal'
+        pet: {
+          name: 'Apple',
+          dob: '2010-03-19',
+          gender: 'F',
+          breed: 'maltese-poodle',
+          color: 'cream',
+          weight: '5lb',
+          neuter: true,
+          microchip: '1CWR4465HS424342EQ',
+          profileUrl: 'http://cl.jroo.me/z3/Q/8/F/e/a.baa-Puppy-or-teddy-bear.jpg'
+        },
+        vet: {
+          practice: 'Charlie loves Dogs',
+          address: {
+            line1: '123 Road',
+            line2: 'Area',
+            city: 'City',
+            state: 'State',
+            country: 'Country',
+          },
+          phone: '12345678910',
+          email: 'vet@email.com',
+          website: 'www.vet.com'
+        },
+        status: {
+          vaccine: 'active',
+          update: 'normal'
+        },
+        vaccine: [
+          {
+            name: 'vaccine1',
+            date_administered: '2014-06-04',
+            date_expired: '2016-10-09'
+          },
+          {
+            name: 'vaccine2',
+            date_administered: '2014-06-04',
+            date_expired: '2016-10-09'
+          },
+          {
+            name: 'vaccine3',
+            date_administered: '2014-06-04',
+            date_expired: '2016-10-09'
+          }
+        ]
       },
       {
-        name: 'Bowser',
-        age: 3,
-        imgUrl: 'http://cl.jroo.me/z3/Q/8/F/e/a.baa-Puppy-or-teddy-bear.jpg',
-        vet: 'Charlie',
-        vaccineStatus: 'expiring',
-        updateStatus: 'pending'
+        pet: {
+          name: 'Bowser',
+          dob: '2010-03-19',
+          gender: 'F',
+          breed: 'maltese-poodle',
+          color: 'cream',
+          weight: '5lb',
+          neuter: true,
+          microchip: '1CWR4465HS424342EQ',
+          profileUrl: 'http://cl.jroo.me/z3/Q/8/F/e/a.baa-Puppy-or-teddy-bear.jpg'
+        },
+        vet: {
+          practice: 'Charlie loves Dogs',
+          address: {
+            line1: '123 Road',
+            line2: 'Area',
+            city: 'City',
+            state: 'State',
+            country: 'Country',
+          },
+          phone: '12345678910',
+          email: 'vet@email.com',
+          website: 'www.vet.com'
+        },
+        status: {
+          vaccine: 'active',
+          update: 'normal'
+        },
+        vaccine: [
+          {
+            name: 'vaccine1',
+            date_administered: '2014-06-04',
+            date_expired: '2016-10-09'
+          },
+          {
+            name: 'vaccine2',
+            date_administered: '2014-06-04',
+            date_expired: '2016-10-09'
+          },
+          {
+            name: 'vaccine3',
+            date_administered: '2014-06-04',
+            date_expired: '2016-10-09'
+          }
+        ]
       },
       {
-        name: 'Baci',
-        age: 4,
-        imgUrl: 'http://cl.jroo.me/z3/Q/8/F/e/a.baa-Puppy-or-teddy-bear.jpg',
-        vet: 'Charlie',
-        vaccineStatus: 'expired',
-        updateStatus: 'normal'
-      }
+        pet: {
+          name: 'Baci',
+          dob: '2010-03-19',
+          gender: 'F',
+          breed: 'maltese-poodle',
+          color: 'cream',
+          weight: '5lb',
+          neuter: true,
+          microchip: '1CWR4465HS424342EQ',
+          profileUrl: 'http://cl.jroo.me/z3/Q/8/F/e/a.baa-Puppy-or-teddy-bear.jpg'
+        },
+        vet: {
+          practice: 'Charlie loves Dogs',
+          address: {
+            line1: '123 Road',
+            line2: 'Area',
+            city: 'City',
+            state: 'State',
+            country: 'Country',
+          },
+          phone: '12345678910',
+          email: 'vet@email.com',
+          website: 'www.vet.com'
+        },
+        status: {
+          vaccine: 'active',
+          update: 'normal'
+        },
+        vaccine: [
+          {
+            name: 'vaccine1',
+            date_administered: '2014-06-04',
+            date_expired: '2016-10-09'
+          },
+          {
+            name: 'vaccine2',
+            date_administered: '2014-06-04',
+            date_expired: '2016-10-09'
+          },
+          {
+            name: 'vaccine3',
+            date_administered: '2014-06-04',
+            date_expired: '2016-10-09'
+          }
+        ]
+      },
     ];
 
     $scope.expand = function(pet) {
