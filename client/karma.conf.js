a94c39d41b7cd718ee8fb1a8bc364379e7483b0f
Taylor Harwin <taylor.harwin@gmail.com>
2014-06-12 18:15:12 -0700

    front-end test scaffold

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -1,5 +1,5 @@
 // Karma configuration
-// Generated on Wed Jun 11 2014 22:06:08 GMT-0700 (PDT)
+// Generated on Thu Jun 12 2014 18:10:49 GMT-0700 (PDT)
 
 module.exports = function(config) {
   config.set({
@@ -10,24 +10,24 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'requirejs'],
+    frameworks: ['mocha'],
 
 
     // list of files / patterns to load in the browser
     files: [
-      'client/test/test.main.js',
-      'client/test/test-main.js',
-      {pattern: 'client/admin/app.js', included: false},
-      {pattern: 'ls', included: false},
-      {pattern: 'client/admin/app/app.js', included: false},
-      {pattern: 'client/user/app/app.js', included: false},
-      {pattern: 'client/admin/app/directives/*.js', included: false}
+      'user/app/*.js',
+      'user/app/**/*.js',
+      'user/app/**/**/*.js',
+      'admin/*.js',
+      'admin/app/*.js',
+      'admin/app/**/*.js',
+      'admin/app/**/**/*.js'
     ],
 
 
     // list of files to exclude
     exclude: [
-      ''''
+      
     ],
 
 
@@ -58,12 +58,12 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    autoWatch: false,
 
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: [],
 
 
     // Continuous Integration mode
