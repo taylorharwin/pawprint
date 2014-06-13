1478b049e4619ce38634be38f04f9b1af3629b1c
Benjamin Lee <leebyp@gmail.com>
2014-06-13 12:28:44 -0700

    Added template controller and template for main

--- client/user/app/pages/controllers/main.ctrl.js
+++ client/user/app/pages/controllers/main.ctrl.js
@@ -3,4 +3,31 @@
 angular.module('user.pages.controllers')
   .controller('MainCtrl', function ($scope) {
     console.log($scope);
+    // @TODO retrieve properly though GET request
+    $scope.pets = [
+      {
+        name: 'Apple',
+        age: 3,
+        imgUrl: 'http://cl.jroo.me/z3/Q/8/F/e/a.baa-Puppy-or-teddy-bear.jpg',
+        vet: 'Charlie',
+        vaccineStatus: 'active',
+        updateStatus: 'normal'
+      },
+      {
+        name: 'Bowser',
+        age: 3,
+        imgUrl: 'http://cl.jroo.me/z3/Q/8/F/e/a.baa-Puppy-or-teddy-bear.jpg',
+        vet: 'Charlie',
+        vaccineStatus: 'expiring',
+        updateStatus: 'pending'
+      },
+      {
+        name: 'Baci',
+        age: 4,
+        imgUrl: 'http://cl.jroo.me/z3/Q/8/F/e/a.baa-Puppy-or-teddy-bear.jpg',
+        vet: 'Charlie',
+        vaccineStatus: 'expired',
+        updateStatus: 'normal'
+      }
+    ];
   });
