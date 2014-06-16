4291a0484fe8cfddbb3419c9b4759dc3b8cddc1e
Taylor Harwin <taylor.harwin@gmail.com>
2014-06-16 10:46:04 -0700

    removes unnecessary function

--- client/admin/app/pages/controllers/request.ctrl.js
+++ client/admin/app/pages/controllers/request.ctrl.js
@@ -20,11 +20,5 @@ angular.module('admin.pages.controllers')
       console.log('Dropdown is now: ', open);
     };
 
-    $scope.toggleDropdown = function ($event) {
-      $event.preventDefault();
-      $event.stopPropagation();
-      $scope.status.isopen = !$scope.status.isopen;
-    };
-
   });
   
