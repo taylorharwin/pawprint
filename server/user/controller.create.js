1fd3d8ad2cc7951a96e9928e56c981bf5b5d9009
Jonathan Huang <jon.hc.huang@gmail.com>
2014-06-16 12:00:53 -0700

    Added routes/logic for saving vet, request

--- server/user/controller.create.js
+++ server/user/controller.create.js
@@ -4,6 +4,8 @@ var User = require('../app/models/user.js'),
     Pets = require('../app/collections/pets.js'),
     Request = require('../app/models/request.js'),
     Requests = require('../app/collections/requests.js'),
+    Vet = require('../app/models/vet.js'),
+    Vets = require('../app/collections/vets.js'),
     Q    = require('q');
 
 // TODO: validations for field length/type
@@ -11,44 +13,52 @@ var User = require('../app/models/user.js'),
 var createPet = function(req, res) {
   // doesn't take into account vaccines
   var userid = req.params.userid;
-  var pet = new Pet(req.body);
   
-  pet.save().then(function(pet) {
+  // create a new pet with userid
+  Pet.forge(req.body).save().then(function(pet) {
+    // attaches pet to user through the user_pet table
     User.forge({id: userid}).pet().attach(pet);
     Pets.add(pet);
-    res.send(200, pet);
+    res.send(201, pet);
   });
 };
 
 var createUser = function(req, res) {
-  var user = new User(req.body); 
   // need to bcrypt at some point
   // figure out logic for breaking up account creation and user details?
-  user.save().then(function(newUser) {
+  User.forge(req.body).save().then(function(newUser) {
     Users.add(newUser);
-    res.send(200, newUser);
+    res.send(201, newUser);
   });
 };
 
 var createRequest = function(req, res) {
   var userid = req.params.userid;
   var petid = req.params.petid;
-  var vet = req.body.vetid;
+  var vet = req.body.vet_id;
 
   var request = new Request({
     user_id: userid,
     pet_id: petid,
-    vet_id: vetid
+    vet_id: vet
   });
 
   request.save().then(function(newRequest) {
     Requests.add(newRequest);
-    res.send(200, newRequest);
+    res.send(201, newRequest);
+  });
+};
+
+var createVet = function(req, res) {
+  Vet.forge(req.body).save().then(function(newVet) {
+    Vets.add(newVet);
+    res.send(201, newVet);
   });
 };
 
 module.exports = exports = {
   createUser : createUser, 
   createPet : createPet,
-  createRequest : createRequest
+  createRequest : createRequest,
+  createVet : createVet
 };
--- server/user/routes.js
+++ server/user/routes.js
@@ -3,13 +3,14 @@ var controllerGet = require('./controller.get.js');
 
 module.exports = exports = function (router) {
 
-  // post requests are the wrong routes
   router.route('/')
     .post(controllerCreate.createUser);
-  router.route('/:userid/pets')
+  router.route('/:userid/pet')
     .post(controllerCreate.createPet);
-  router.route('/:userid/pets/:petid/request')
+  router.route('/:userid/pet/:petid/request')
     .post(controllerCreate.createRequest);
+  router.route('/vet')
+    .post(controllerCreate.createVet);
 
   // router.route('/:userid')
   //   .get(controllerGet.getUser);
