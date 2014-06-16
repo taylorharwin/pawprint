22247bb75e0827430097069e2ce3ac11cdf8938f
Benjamin Lee <leebyp@gmail.com>
2014-06-16 15:41:48 -0700

    Vet services to communicate with server w/ dummy data for the time being

--- client/user/app/pages/services/vet.srv.js
+++ client/user/app/pages/services/vet.srv.js
@@ -3,11 +3,44 @@ angular.module('user.pages.services')
   .factory('VetFactory', function ($http) {
 
     //@TODO test all the functions in this factory
-    function getVets (){
+    //@TODO use these functions to request API:
+    function getVets () {
       return Restangular.all('vets').getList().then(function (vets){
-        $scope.vets = vets;
         return vets;
       });
+    }
+
+    //==========================================//
+    //@NOTE dummy functions returning dummy data//
+    //==========================================//
+
+    function getVets () {
+      return [
+        {
+          practiceName: 'abc',
+          website: 'abc',
+          streetAddress: 'abc',
+          city: 'abc',
+          zip: '123',
+          phone: '123'
+        },
+        {
+          practiceName: 'abc',
+          website: 'abc',
+          streetAddress: 'abc',
+          city: 'abc',
+          zip: '123',
+          phone: '123'
+        },
+        {
+          practiceName: 'abc',
+          website: 'abc',
+          streetAddress: 'abc',
+          city: 'abc',
+          zip: '123',
+          phone: '123'
+        }
+      ];
     }
 
     return {
