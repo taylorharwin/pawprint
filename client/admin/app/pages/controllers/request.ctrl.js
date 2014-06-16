d18fa356c73b31ff859597d2f6fc69fa6adbc4e1
Taylor Harwin <taylor.harwin@gmail.com>
2014-06-16 10:42:23 -0700

    adds bootstrap UI for vaccines list

--- client/admin/app/pages/controllers/request.ctrl.js
+++ client/admin/app/pages/controllers/request.ctrl.js
@@ -4,6 +4,27 @@ angular.module('admin.pages.controllers')
   .controller('RequestCtrl', function ($scope, reqIDFactory) {
 
     $scope.reqID = reqIDFactory.getRequestID();
-    
+
+    $scope.vaccines = [
+      'Rabies',
+      'Feline Leukemia',
+      'Parvovirus',
+      'Rabbit anti-depressant'
+    ];
+
+    $scope.status = {
+      isopen: false
+    };
+
+    $scope.toggled = function (open) {
+      console.log('Dropdown is now: ', open);
+    };
+
+    $scope.toggleDropdown = function ($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+      $scope.status.isopen = !$scope.status.isopen;
+    };
+
   });
   
