0d0c508f90e616730480de456180c824df0c20c2
Jonathan Huang <jon.hc.huang@gmail.com>
2014-06-16 11:57:05 -0700

    Updated schema to include status for request Please enter the commit message for your changes. Lines starting

--- server/app/db_config.js
+++ server/app/db_config.js
@@ -108,6 +108,7 @@ db.knex.schema.hasTable('request').then(function(exists) {
       request.integer('user_id');
       request.integer('pet_id');
       request.integer('vet_id');
+      request.string('status', 20);
       request.timestamps();
     }).then(function (table) {
       console.log('Created Table', table);
