2461689433dae2db1862a9040cf3fd8eb1a5b28c
Benjamin Lee <leebyp@gmail.com>
2014-06-16 15:40:29 -0700

    User services to communicate with server w/ dummy data for the time being

--- client/user/app/pages/services/user.srv.js
+++ client/user/app/pages/services/user.srv.js
@@ -1,6 +1,10 @@
+//=============================================
+//@TODO split into user.srv.js && pet.srv.js
+//=============================================
+
 angular.module('user.pages.services')
 
-  .factory('UserFactory', function (Restangular, $scope, $http){
+  .factory('UserFactory', function (Restangular, $http){
 
     var baseUrl = 'http://127.0.0.1:8000/';
     var config = {
@@ -10,13 +14,36 @@ angular.module('user.pages.services')
     };
 
     //@TODO test all the functions in this factory
-    function newUser (data) {
+    //@TODO use these functions to request API:
+    function setNewUser (data) {
       var url = baseUrl + 'user';
       return $http.post(url, data, config);
     }
 
+    function setNewUserPet (data) {
+      var url = baseUrl + 'pet';
+      return $http.post(url, data, config);
+    }
+
+    function setUserEdit (userId) {
+      var url = baseUrl + 'user';
+      return $http.put(url, data, config); 
+    }
+
+    function setUserPetEdit (userId, petId) {
+      var url = baseUrl + 'pet';
+      return $http.put(url, data, config); 
+    }
+
+    function getUser (userId){
+      return Restangular.one('user', userId).get().then(function (user){
+        return user;
+      });
+    }
+
     function getUserPet (userId, petId){
       return Restangular.one('user', userId).one('pet', petId).get().then(function (pet){
+        pet.edit = false;
         return pet;
       });
     }
@@ -27,20 +54,157 @@ angular.module('user.pages.services')
       });
     }
 
-    function getUser (userId){
-      return Restangular.one('user', userId).get().then(function (user){
-        $scope.user = user;
-        var pets = getUserPets(userId, user.pets);
-        $scope.pets = pets;
-        return user;
-      });
+
+    //==========================================//
+    //@NOTE dummy functions returning dummy data//
+    //==========================================//
+    function getUser () {
+      return {
+        user: {
+          id: '123',
+          email: 'apple@hackreactor.com',
+          firstName: 'Benjamin',
+          lastName: 'Lee',
+          streetAddress: 'Pineapple Street',
+          city: 'SF',
+          state: 'CA',
+          zip: '41212',
+          phone: '12313131'
+        },
+        pet: ['petId1', 'petId2'],
+        request: ['requestId1', 'requestId1']
+      };
+    }
+
+    function getUserPets () {
+      return [
+        {
+          pet: {
+            id: '123',
+            name: 'Apple',
+            birthdate: '2010-03-19',
+            gender: 'F',
+            breed: 'maltese-poodle',
+            color: 'cream',
+            weight: '5lb',
+            neuter: true,
+            microchip: '1CWR4465HS424342EQ',
+            profileUrl: 'http://cl.jroo.me/z3/Q/8/F/e/a.baa-Puppy-or-teddy-bear.jpg'
+          },
+          vaccine: [
+            {
+              id: '456',
+              name: 'bordetella',
+              expiration: '3years',
+              dateAdministered: '2013-10-09',
+              dateExpired: '2014-10-09'
+            }
+          ],
+          status: {
+            vaccine: 'active',
+            update: 'normal'
+          },
+          edit: false
+        },
+        {
+           pet: {
+            id: '456',
+            name: 'Apple',
+            birthdate: '2010-03-19',
+            gender: 'F',
+            breed: 'maltese-poodle',
+            color: 'cream',
+            weight: '5lb',
+            neuter: true,
+            microchip: '1CWR4465HS424342EQ',
+            profileUrl: 'http://cl.jroo.me/z3/Q/8/F/e/a.baa-Puppy-or-teddy-bear.jpg'
+          },
+          vaccine: [
+            {
+              id: '456',
+              name: 'bordetella',
+              expiration: '3years',
+              dateAdministered: '2013-10-09',
+              dateExpired: '2014-10-09'
+            }
+          ],
+          status: {
+            vaccine: 'expiring',
+            update: 'normal'
+          },
+          edit: false
+        },
+        {
+           pet: {
+            id: '789',
+            name: 'Apple',
+            birthdate: '2010-03-19',
+            gender: 'F',
+            breed: 'maltese-poodle',
+            color: 'cream',
+            weight: '5lb',
+            neuter: true,
+            microchip: '1CWR4465HS424342EQ',
+            profileUrl: 'http://cl.jroo.me/z3/Q/8/F/e/a.baa-Puppy-or-teddy-bear.jpg'
+          },
+          vaccine: [
+            {
+              id: '456',
+              name: 'bordetella',
+              expiration: '3years',
+              dateAdministered: '2013-10-09',
+              dateExpired: '2014-10-09'
+            },
+            {
+              id: '456',
+              name: 'rabies',
+              expiration: '3years',
+              dateAdministered: '2013-10-09',
+              dateExpired: '2014-10-09'
+            }
+          ],
+          status: {
+            vaccine: 'expired',
+            update: 'pending'
+          },
+          edit: false
+        }
+      ];
+    }
+
+    function setUserEdit () {
+      //send a request to update User info
+      console.log('UserEdit sent');
+    }
+
+    function setUserPetEdit () {
+      //send a request to update pet details
+      console.log('UserPetEdit sent');
+    }
+
+    function setUserPetUpdate () {
+      //send a request to update info from vet
+      console.log('UserPetUpdate sent');
+    }
+
+    function cancelUserPetUpdate () {
+      //ask to cancel latest update regardless
+     console.log('UserPetUpdate cancelled'); 
     }
 
     return {
-      newUser: newUser,
+      setNewUser: setNewUser,
+      setNewUserPet: setNewUserPet,
+      
+      getUser: getUser,
       getUserPet: getUserPet,
-      getuserPets: getUserPets,
-      getuser: getUser
+      getUserPets: getUserPets,
+
+      setUserEdit: setUserEdit,
+      setUserPetEdit: setUserPetEdit,
+      setUserPetUpdate: setUserPetUpdate,
+
+      cancelUserPetUpdate: cancelUserPetUpdate
     };
 
   });
