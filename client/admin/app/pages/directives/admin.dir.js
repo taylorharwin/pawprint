720f6b791ce3edee3529787dd059c814fcae4ab5
Taylor Harwin <taylor.harwin@gmail.com>
2014-06-16 17:15:21 -0700

    adds three most simplisitic directives

--- client/admin/app/pages/directives/admin.dir.js
+++ client/admin/app/pages/directives/admin.dir.js
@@ -2,6 +2,26 @@
 
 angular.module('admin.pages.directives')
 
-  .directive('vetInfoDirective', function () {
-    return '<div> I am a shared Directive </div>';
+  .directive('vetInfo', function () {
+    return {restrict: 'AE',
+     replace: 'true',
+     template: '<h3>Vet Info Directive!!</h3>'};
+  })
+
+  .directive('userInfo', function () {
+    return {restrict: 'AE',
+      replace: 'true',
+      template: '<h3>Customer Info Directive!!</h3>'};
+  })
+
+  .directive('vaccRecord', function () {
+    return {restrict: 'AE',
+      replace: 'true',
+      template: '<h3>Vaccine Records Directive!!</h3>'};
+  })
+
+  .directive('contactHist', function () {
+    return {restrict: 'AE',
+    replace: 'true',
+    template: '<h3>Contact History Directive!!</h3>'};
   });
