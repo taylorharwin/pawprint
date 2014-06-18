3d37ef9c29b43addd75e4a46d4eede6162df1674
emiidee <emily.y.dong@gmail.com>
2014-06-18 15:58:43 -0700

    Reorganized routes, no code edited

--- server/user/routes.js
+++ server/user/routes.js
@@ -7,26 +7,28 @@ module.exports = exports = function (router) {
 
   router.route('/')
     .post(Create.createUser);
-  router.route('/:userid/pet')
-    .post(Create.createPet);
-  router.route('/:userid/pet/:petid/request')
-    .post(Create.createRequest);
-  router.route('/vet')
-    .post(Create.createVet);
-
   router.route('/:userid')
     .put(Put.putUser)
     .get(Get.getUser);
-  router.route('/:userid/pet/:petid')
-    .put(Put.putPet);
-  router.route('/user/:userid/pet/:petid/request/:requestid')
-    .delete(Delete.deleteRequest);
 
+  router.route('/:userid/pet')
+    .post(Create.createPet);
   router.route('/:userid/pets')
     .get(Get.getPets);
-  router.route('/:userid/requests')
-    .get(Get.getRequests);
+  router.route('/:userid/pet/:petid')
+    .put(Put.putPet);
+
   router.route('/:userid/pet/:petid/vaccines')
     .get(Get.getVaccines);
+
+  router.route('/:userid/requests')
+    .get(Get.getRequests);
+  router.route('/:userid/pet/:petid/request')
+    .post(Create.createRequest);
+  router.route('/user/:userid/pet/:petid/request/:requestid')
+    .delete(Delete.deleteRequest);
+  
+  router.route('/vet')
+    .post(Create.createVet);
 };
 
