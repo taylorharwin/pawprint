1457e19f7c41fe3bbbe0d5ccd6eb20bb22c49ff4
Benjamin Lee <leebyp@gmail.com>
2014-06-18 14:23:27 -0700

    Changed controllers to not use dummy data and adjust for changes in html and services

--- client/user/app/pages/controllers/login.ctrl.js
+++ client/user/app/pages/controllers/login.ctrl.js
@@ -1,6 +1,36 @@
-'use strict';
-
 angular.module('user.pages.controllers')
-  .controller('LoginCtrl', function ($scope) {
+  .controller('LoginCtrl', function ($scope, UserFactory, CurrentUserFactory) {
     console.log($scope);
+
+    $scope.user = {
+      email: '',
+      password: ''
+    };
+
+    //not 100% sure about promises here
+    // $scope.userSignup = function () {
+    //   UserFactory.postUserLogin($scope.user).then(function (response) {
+    //     CurrentUserFactory.setUserId(res.body.id);
+    //     return $q.defer().promise;
+    //   }, function (response) {
+    //     console.log('Error with status code', response.status);
+    //     $scope.signupError = true;
+    //     return;
+    //   }).then(function (){
+    //     $state.go('app.main');
+    //   });
+    // };
+
+    $scope.userLogin = function () {
+      console.log('function userLogin running...');
+      UserFactory.postUserLogin($scope.user).then(function (response) {
+        console.log('successful post to server');
+        CurrentUserFactory.setUserId(response.body.id);
+        $state.go('app.main');
+      }, function (response) {
+        console.log('Error with status code', response.status);
+        $scope.signupError = true;
+      });
+    };
+    
   });
