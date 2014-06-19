d3230b0aa787586b5aa0f6477be1ca4973247352
emiidee <emily.y.dong@gmail.com>
2014-06-19 07:45:53 -0700

    Restructured admin routes per route 2.0 doc

--- server/admin/routes.js
+++ server/admin/routes.js
@@ -1,42 +1,56 @@
 var Create = require('./controller.create.js');
 var Get = require('./controller.get.js');
 var Put = require('./controller.put.js');
+// var Delete = require('./controller.delete.js'); // TODO
 
 module.exports = exports = function (router) {
-  // create admin route
+  // router.route('/:adminid/requests')
+  //   .post(Post.adminLogin);
+
   router.route('/:adminid/requests')
     .get(Get.getRequests);
+
   router.route('/:adminid/requests/:requestid')
-    .get(Get.getRequest);
+    .get(Get.getRequest); // TODO deprecate this route
+    // .put(Put.putRequest); // TODO
+
   router.route('/:adminid/pets/:petid')
     .get(Get.getPet);
   router.route('/:adminid/users/:userid')
     .get(Get.getUser);
   router.route('/:adminid/vets/:vetid')
     .get(Get.getVet);
-  router.route('/:adminid/vets/:vetid/contacts')
-    .get(Get.getVetContacts);
-  router.route('/:adminid/requests/:requestid/vaccines')
-    .get(Get.getVaccines);
-  router.route('/:adminid/requests/:requestid/logs')
-    .get(Get.getLogs);
-  router.route('/:adminid/requests/:requestid/pdfs')
-    .get(Get.getPDFs);
-  
-  router.route('/:adminid/requests/:requestid/contacts')
-    .post(Create.createContact);
-  router.route('/:adminid/vets/:vetid/contacts')
-    .post(Create.createVetContact);
-  router.route('/:adminid/requests/:requestid/pdf')
-    .post(Create.createPdf);
-  
-  router.route('/:adminid/requests/:requestid/vaccines')
-    .post(Create.createVaccine)
-    .put(Put.putVaccine);
-  router.route('/:adminid/requests/:requestid/contacts/:contactid')
-    .put(Put.putContact);
-};
 
-// PUT /admin/:adminid/request/:requestid/contact/:id  update contact log  {contactID: {contact type: xxx, admin: xxx, ...}}
-// PUT /admin/:adminid/request/:requestid/vaccines update vaccine entries [array]  {vaccineID: {vaccine: xxx, date administered:xxx, ... }}
-// PUT /admin/:adminid/vet/:vetid/vetcontact/:id update vet contact  {vaccineID: {name: xxx, vetID:xxx, ...}}
+  router.route('/:adminid/requests/:requestid/vaccines')
+    .get(Get.getVaccines)
+    // .post(Create.createVaccine); // TODO
+  // router.route('/:adminid/requests/:requestid/vaccines/:vaccineID')
+    // .put(Put.putVaccine) // TODO
+    // .delete(Delete.deleteVaccine); // TODO
+
+  router.route('/:adminid/requests/:requestid/logs')
+    .get(Get.getLogs)
+    .post(Create.createContact); // TODO: change name to 'createLog'
+  router.route('/:adminid/requests/:requestid/logs/:logid')
+    .put(Put.putContact) // TODO: change name to 'putLog'
+    // .delete(Delete.deleteLog); // TODO
+
+  router.route('/:adminid/requests/:requestid/pdfs')
+    .get(Get.getPDFs)
+    .post(Create.createPdf);
+  // router.route('/:adminid/requests/:requestid/pdfs/:pdfid')
+    // .put(Put.putPdf) // TODO
+    // .delete(Delete.deletePdf); // TODO
+
+  router.route('/:adminid/vets/:vetid/contacts')
+    .get(Get.getVetContacts) // TODO: change to 'getContacts'
+    .post(Create.createVetContact); // TODO: change to 'createContacts'
+  // router.route('/:adminid/vets/:vetid/contacts/:contactid')
+  //   .put(Put.putContact) // TODO
+  //   .delete(Delete.deleteContact); // TODO
+
+  router.route('/:adminid/requests/:requestid/vaccines')
+    // .get(Get.getVaccines) //TODO
+    .post(Create.createVaccine)
+    .put(Put.putVaccine); // TODO: deprecate this route?
+};
