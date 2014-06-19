b80d6b5a77545776fe6a877e28c95d7a3cbb3018
emiidee <emily.y.dong@gmail.com>
2014-06-19 07:08:13 -0700

    Reorganized user routes and inserted comments for routes to be built

--- server/user/routes.js
+++ server/user/routes.js
@@ -8,27 +8,28 @@ module.exports = exports = function (router) {
   router.route('/')
     .post(Create.createUser);
   router.route('/:userid')
+    .get(Get.getUser)
     .put(Put.putUser)
-    .get(Get.getUser);
+    // .delete(Delete.deleteUser);
 
-  router.route('/:userid/pet')
-    .post(Create.createPet);
   router.route('/:userid/pets')
-    .get(Get.getPets);
-  router.route('/:userid/pet/:petid')
-    .put(Put.putPet);
+    .get(Get.getPets)
+    .post(Create.createPet);
+  router.route('/:userid/pets/:petid')
+    .put(Put.putPet)
+    // .delete(Delete.deletePet);
 
-  router.route('/:userid/pet/:petid/vaccines')
+  router.route('/:userid/pets/:petid/vaccines')
     .get(Get.getVaccines);
 
-  router.route('/:userid/requests')
-    .get(Get.getRequests);
-  router.route('/:userid/pet/:petid/request')
+  router.route('/:userid/pets/:petid/requests')
+    .get(Get.getRequests) // change this to get requests per pet
     .post(Create.createRequest);
-  router.route('/:userid/pet/:petid/request/:requestid')
+  router.route('/:userid/pets/:petid/requests/:requestid')
     .delete(Delete.deleteRequest);
   
-  router.route('/vet')
-    .post(Create.createVet);
+  router.route('/vets')
+    // .get(Get.getVets)
+    .post(Create.createVet); // Change this to auto happen on the admin side?
 };
 
