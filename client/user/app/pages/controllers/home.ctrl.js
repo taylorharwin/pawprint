cf3c9d1acf3330eb8f832c9f5e934ac2de36a7ab
Benjamin Lee <leebyp@gmail.com>
2014-06-19 01:18:59 -0700

    CurrentUserFactory keeps track of currentuser (later Auth) and controllers wait for response with promises

--- client/user/app/pages/controllers/home.ctrl.js
+++ client/user/app/pages/controllers/home.ctrl.js
@@ -1,5 +1,3 @@
-'use strict';
-
 angular.module('user.pages.controllers')
   .controller('HomeCtrl', function ($scope) {
     console.log($scope);
--- client/user/app/pages/services/currentuser.srv.js
+++ client/user/app/pages/services/currentuser.srv.js
@@ -3,7 +3,6 @@ angular.module('user.pages.services')
   .factory('CurrentUserFactory', function () {
 
     var userId = null;
-    var petIds = [];
 
     function setUserId (id) {
       userId = id;
@@ -13,28 +12,9 @@ angular.module('user.pages.services')
       return userId;
     }
 
-    // function addPetId (id) {
-    //   petIds.push(id);
-    // }
-
-    // function removePetId (id) {
-    //   for (var i=0; i<petIds.length; i++) {
-    //     if (petIds[i] === id) {
-    //       petIds.splice(i, 1);
-    //     }
-    //   }
-    // }
-
-    // function getPetIds () {
-    //   return petIds;
-    // }
-
     return {
       setUserId: setUserId,
-      getUserId: getUserId,
-      // addPetId: addPetId,
-      // getPetIds: getPetIds
-      // removePetId: removePetId,
+      getUserId: getUserId
     };
 
   });
