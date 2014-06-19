cc4a919cdb6db4b606872f99998bcce89ca4735f
Benjamin Lee <leebyp@gmail.com>
2014-06-19 01:22:31 -0700

    Altered profile html and controllers handling update response

--- client/user/app/pages/controllers/profile.ctrl.js
+++ client/user/app/pages/controllers/profile.ctrl.js
@@ -1,23 +1,39 @@
 angular.module('user.pages.controllers')
-  .controller('ProfileCtrl', function ($scope, UserFactory, CurrentUserFactory) {
+  .controller('ProfileCtrl', function ($scope, $state, UserFactory, CurrentUserFactory) {
     console.log($scope);
 
     $scope.userId = CurrentUserFactory.getUserId();
 
     UserFactory.getUser($scope.userId).then(function (data) {
-      $scope.user.user = data;
+      $scope.user = data;
       // console.log($scope.user, 'beginning');
     });
 
-    $scope.save = function() {
-      // console.log($scope.user, 'prePUT');
-      $scope.user.put().then(function (){
+    $scope.updateUser = function() {
+      // console.log($scope.user, '=========');
+      $scope.user.put().then(function (response){
         console.log('successful put');
-        $scope.update = true;
+        // console.log(response);
+        //update the scope user to reflect db
+        $scope.user = response;
+        $scope.updateSuccess = true;
+        $scope.updateError = false;
       }, function (response) {
-        console.log('Error with response status code', response.status);
-        $scope.update = false;
+        console.log('Error with request');
+        $scope.updateSuccess = false;
+        $scope.updateError = true;
       });
     };
     
+    $scope.deleteUser = function() {
+      UserFactory.deleteUser($scope.userId).then(function (response) {
+        console.log('deleted user');
+        // @NOTE delete the local auth settings
+        CurrentUserFactory.setUserId('');
+        $state.go('public.home');
+      }, function (response) {
+        console.log('Error with request');
+      });
+    };
+
   });
