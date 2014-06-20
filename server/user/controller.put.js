221acfd00a63e4917933bc7f259da2b14b4ab72d
emiidee <emily.y.dong@gmail.com>
2014-06-19 17:21:49 -0700

    Refactored PUT requests to use promises to ensure the correct, updated model is sent back

--- server/user/controller.put.js
+++ server/user/controller.put.js
@@ -9,16 +9,18 @@ var User = require('../app/models/user.js'),
 var putUser = function(req, res) {
   var userid = req.params.userid;
   User.forge({id: userid}).fetch().then(function(user) {
-    user.save(req.body);
-    res.send(200, user);
+    return user.save(req.body);
+  }).then(function(model) {
+    res.send(200, model);
   });
 };
 
 var putPet = function(req, res) {
   var petid = req.params.petid;
   Pet.forge({id: petid}).fetch().then(function(pet) {
-    pet.save(req.body);
-    res.send(200, pet);
+    return pet.save(req.body);
+  }).then(function(model) {
+    res.send(200, model);
   });
 };
 
