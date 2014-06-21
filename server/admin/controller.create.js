dd2944719ea514635c2298d19939b5d6da66d7e7
emiidee <emily.y.dong@gmail.com>
2014-06-21 01:21:50 -0700

    Refactored creator to return a function
    This eliminates the need to pass req and res each time the function is
    called

--- server/admin/controller.create.js
+++ server/admin/controller.create.js
@@ -20,7 +20,7 @@ var db                = require('../app/db_config.js'),
     Q                 = require('q');
 
 /************DOCS****************/
-// _creator makes POST request with the req.body extended by any params passed in
+// _creator returns a function that makes POST request with the req.body extended by any params passed in
 // params is an object with properties for each property the model should be extended with
   // Example: 
     // var createLog = function(req, res) {
@@ -40,18 +40,23 @@ var db                = require('../app/db_config.js'),
     //     request_id: req.params.requestid
     //   });
     // }; 
-var _creator = function(req, res, Model, params) {
-  var newObj = req.body;
-  for (var property in params) {
-    newObj[property] = params[property];
-  }
+var _creator = function(Model, params) {
+  return function (req, res) {
+    var newObj = req.body;
+    for (var property in params) {
+      newObj[property] = req.params[property];
+    }
 
-  Model.forge(newObj).save().then(function(model) {
-    res.send(201, model);
-  });
-  // TODO add error handling
+    Model.forge(newObj).save().then(function(model) {
+      res.send(201, model);
+    }).catch(function(err) {
+      console.error(err);
+      res.send(500, 'Internal server error');
+    });
+  };
 };
 
+// createPetVaccine
 var createPetVaccine = function(req, res) {
   var requestid = req.params.requestid;
   var vaccines = req.body;
@@ -73,22 +78,16 @@ var createPetVaccine = function(req, res) {
     });
 };
 
-var createVaccine = function(req, res) {
-  _creator(req, res, Vaccine);
-};
+var createVaccine = _creator(Vaccine);
 
-var createLog = function(req, res) {
-  _creator(req, res, ContactHistory, {
-    admin_id: req.params.adminid,
-    request_id: req.params.requestid
-  });
-};
+var createLog = _creator(ContactHistory, {
+  admin_id: 'adminid',
+  request_id: 'requestid'
+});
 
-var createVetContact = function(req, res) {
-  _creator(req, res, VetContact, {
-    vet_id: req.params.vetid
-  });
-};
+var createVetContact = _creator(VetContact, {
+  vet_id: 'vetid'
+});
 
 var createPdf = function(req, res) {
   // TODO, this will be a multi-part form request
