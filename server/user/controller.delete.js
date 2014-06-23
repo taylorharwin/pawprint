e4fe508cc71110448e6a90564d62da8ac4c4c626
emiidee <emily.y.dong@gmail.com>
2014-06-23 15:43:30 -0700

    Began making deleter functions

--- server/user/controller.delete.js
+++ server/user/controller.delete.js
@@ -1,6 +1,32 @@
-var Request = require('../app/models/request.js'),
-    Q    = require('q');
+var User      = require('../app/models/user.js'),
+    Pet       = require('../app/models/pet.js'),
+    User_Pet  = require('../app/models/user_pet.js'),
+    Request   = require('../app/models/request.js'),
+    Vet       = require('../app/models/vet.js'),
+    db        = require('../app/db_config.js'),
+    Utils     = require('../app/utils.js'),
+    Q         = require('q');
 
+// Changes user status to deactivated, DO NOT actually delete the user
+var deleteUser = Utils.updater(User, {
+  id: 'userid',
+  params: {
+    status: 'deactivated'
+  },
+  omit: ['password', 'salt', 'jwt', 'type']
+});
+
+// Disconnect user and pet, DO NOT actually delete the pet
+var deletePet = function(req, res) {
+  // NEED USER PET MODEL
+  // This can be refactored to use Utils.deleter if deleter takes multiple query options
+  User_Pet.forge({pet_id: req.params.petid, user_id: req.params.userid})
+    .fetch().then(function(model) {
+      model.destroy(res.send(200, model));
+    });
+};
+
+// Updates the request status to canceled, DO NOT actually delete the request
 var deleteRequest = function(req, res) {
   var requestid = req.params.requestid;
 
@@ -13,5 +39,7 @@ var deleteRequest = function(req, res) {
 };
 
 module.exports = exports = {
+  deleteUser : deleteUser,
+  deletePet : deletePet,
   deleteRequest : deleteRequest
 };
