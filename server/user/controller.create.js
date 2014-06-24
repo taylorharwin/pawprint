216c47cc4b2b0c496b384bca7ba7a72e97092e87
Jonathan Huang <jon.hc.huang@gmail.com>
2014-06-23 20:48:15 -0700

    Fixed issues from Emily.

--- server/user/controller.create.js
+++ server/user/controller.create.js
@@ -32,15 +32,14 @@ var createPet = function(req, res) {
     return User_Pet.forge({user_id: newuser.id, pet_id: newpet.id}).fetch();
   })
   .then(function(join) {
-    console.log(join);
     if (!join) throw new Error('Bad Join');
     var date = new Date();
     join.save({created_at: date, updated_at: date}, {patch: true});
     res.send(201, newpet);
   })
   .catch(function(err){
-    console.log(err);
-    res.send(400, err);
+    console.error(err);
+    res.send(500, 'Internal Server Error');
   });
 };
 
