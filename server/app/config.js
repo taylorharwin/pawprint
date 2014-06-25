e3ffff7e4f7d817e0cb3c312a5c16e04c645dde4
Jonathan Huang <jon.hc.huang@gmail.com>
2014-06-24 17:15:15 -0700

    Fixed white space in config.js

--- server/app/config.js
+++ server/app/config.js
@@ -16,7 +16,6 @@ module.exports = exports = function (app, express, routers) {
   app.use(middle.logError);
   app.use(middle.handleError);
   // Set up routes
-
   app.use(express.static(__dirname+'/../../client/user'));
   app.use('/admin', express.static(__dirname+'/../../client/admin'));
   app.use(express.static(__dirname+'/../../client'));
