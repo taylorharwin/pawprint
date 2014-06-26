4d232e2d3284580f2a39c4bcc53912f952faea74
Benjamin Lee <leebyp@gmail.com>
2014-06-25 23:37:39 -0700

    Added auth service to look into cookies

---
+++ client/user/app/common/services/auth.srv.js
@@ -0,0 +1,28 @@
+angular.module('user.common.services')
+  .service('AuthService', function ($cookies) {
+
+    $cookies.loggedin = false;
+    $cookies.token = undefined;
+    $cookies.userId = undefined;
+
+    function getCookie () {
+      return $cookies;
+    }
+
+    function login (token, userId) {
+      $cookies.loggedin = true;
+      $cookies.token = token;
+      $cookies.userId = userId;
+    }
+
+    function logout () {
+      $cookies.loggedin = false;
+      delete $cookies.token;
+      delete $cookies.userId;
+    }
+
+    this.getCookie = getCookie;
+    this.login = login;
+    this.logout = logout;
+
+  });
