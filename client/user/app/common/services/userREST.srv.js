41c75bcc6eebd263ffa7d1b481e648cfe6081127
Benjamin Lee <leebyp@gmail.com>
2014-06-25 23:42:13 -0700

    Separated userREST routes into login and signup

--- client/user/app/common/services/userREST.srv.js
+++ client/user/app/common/services/userREST.srv.js
@@ -2,20 +2,9 @@ angular.module('user.common.services')
 
   .service('UserRESTService', function (Restangular){
 
-    function postUserSignup (data) {
-      // return Restangular.all('user/signup').post(data);
-      return Restangular.all('user').post(data);
+    function postUser(data, type) {
+      return Restangular.all('user/' + type).post(data);
     }
-
-    function postUserLogin (data) {
-      // return Restangular.all('user/login').post(data);
-      return Restangular.all('user').post(data);
-    }
-
-    //@TODO, use this instead when auth is working,
-    // function postUser(data, type) {
-    //   return Restangular.all('user/' + type).post(data);
-    // }
 
     function getUser (id) {
       return Restangular.one('user', id).get();
@@ -25,9 +14,8 @@ angular.module('user.common.services')
       return Restangular.one('user', id).remove();
     }
 
-      this.postUserSignup = postUserSignup;
-      this.postUserLogin = postUserLogin;
-      this.getUser = getUser;
-      this.deleteUser = deleteUser;
+    this.postUser = postUser;
+    this.getUser = getUser;
+    this.deleteUser = deleteUser;
 
   });
