1d3e6429a838ea06ccfcdfdbb7da0e1659f5f9b9
Benjamin Lee <leebyp@gmail.com>
2014-06-26 17:04:11 -0700

    Added login capabilities for admin clientside

--- client/admin/app/common/services/currentadmin.srv.js
+++ client/admin/app/common/services/currentadmin.srv.js
@@ -1,6 +1,6 @@
 angular.module('admin.common.services')
 
-  .service('CurrentAdminService', function () {
+  .service('CurrentAdminService', function (Restangular, AuthService) {
 
     var currentAdmin = null;
 
@@ -12,7 +12,21 @@ angular.module('admin.common.services')
       return currentAdmin;
     }
 
+    function login (admin) {
+      Restangular.all('admin/login').post(admin)
+        .then(function (response) {
+          AuthService.login(response.token, response.id);
+          //temporary to make sure everything still works
+          currentAdmin = response.id;
+          $rootScope.$state.go('app.allRequests');
+        }, function (error) {
+          console.log(error);
+          AuthService.logout();
+        });
+    }
+
     this.setAdminId = setAdminId;
     this.getAdminId = getAdminId;
+    this.login = login;
 
   });
