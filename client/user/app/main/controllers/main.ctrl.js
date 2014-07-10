0e87fc5a2aab580345cb77d96e1fa9732c8f8e47
emiidee <emily.y.dong@gmail.com>
2014-07-10 13:30:03 -0700

    Reformatted pet card layout and styled

--- client/user/app/main/controllers/main.ctrl.js
+++ client/user/app/main/controllers/main.ctrl.js
@@ -13,12 +13,16 @@ angular.module('user.main.controllers')
       });
     
     CurrentPetsService.retrievePets($scope.userId)
-      .then(function (pets) {
+      .then(function(pets) {
+        // TO BE REMOVED
+        var statuses = ['active', 'expiring', 'expired'];
         // Set default profilePic if none exists, todo: move this logic to the server
         for (var pet = 0; pet < pets.pets.length; pet++) {
           if(!pets.pets[pet].profilePic) {
             pets.pets[pet].profilePic = 'assets/img/default.jpeg';
           }
+          // FOR TESTING PURPOSES ONLY, REMOVE WHEN FUNCTIONALITY ADDED
+          pets.pets[pet].vaccineStatus = statuses[Math.round(Math.random() * 2)];
         }
         $scope.pets = pets;
       });
